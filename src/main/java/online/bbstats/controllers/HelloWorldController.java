f7b89bab770a5be8bd87092ec15feebeeb6e5483
rstruthers1 <rstruthers1@yahoo.com>
2015-12-05 16:37:54 -0600

    Initial commit of bbstats!

---
+++ src/main/java/online/bbstats/controllers/HelloWorldController.java
@@ -0,0 +1,40 @@
+package online.bbstats.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+   @RequestMapping(value = "/hello", method = RequestMethod.GET)
+   public ModelAndView hello() {
+       ModelAndView mav = new ModelAndView();
+       mav.setViewName("hello");
+        
+       String str = "Hello World!";
+       mav.addObject("message", str);
+
+       return mav;
+   }
+   
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public ModelAndView welcome() {
+       ModelAndView mav = new ModelAndView();
+       mav.setViewName("welcome");
+        
+
+       return mav;
+   }
+   
+   @RequestMapping(value = "/bbstats", method = RequestMethod.GET)
+   public ModelAndView bbstats() {
+       ModelAndView mav = new ModelAndView();
+       mav.setViewName("bbstats");
+        
+
+       return mav;
+   }
+}
---
+++ src/main/webapp/WEB-INF/jsp/bbstats.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>bbstats</title>
+</head>
+<body>
+    <h1>bbstats!</h1>
+</body>
+</html>
